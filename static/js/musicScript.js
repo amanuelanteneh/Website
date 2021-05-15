8c4c2caeca7b0ec373999b8efb2a68cac0d1889a
amanuelanteneh <asa2rc@virginia.edu>
2021-05-15 16:31:01 -0400

    music

--- static/js/musicScript.js
+++ static/js/musicScript.js
@@ -3,7 +3,8 @@ var albumPics = ["/static/img/the1975.png","/static/img/muse.png",
                  "/static/img/peep.png","/static/img/mac.png",
                  "/static/img/glass.png","/static/img/tool.png",
                  "/static/img/uzi.png", "/static/img/cudi.png",
-                 "/static/img/swae.png"];
+                 "/static/img/swae.png", "/static/img/migos.png",
+                 "/static/img/ocean.png", "/static/img/neighbourhood.png"];
 var albumPicElements = document.getElementsByClassName("album-pic");
 
 
