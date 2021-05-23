4c17f31354d59b50da5dfe241c03bee5e081d787
amanuelanteneh <asa2rc@virginia.edu>
2021-05-23 12:30:15 -0400

    more music

--- static/js/musicScript.js
+++ static/js/musicScript.js
@@ -5,7 +5,9 @@ var albumPics = ["/static/img/the1975.png","/static/img/loveitwhenu.png","/stati
                  "/static/img/sr3mm.png", "/static/img/cultureii.png",
                  "/static/img/blond.png","/static/img/iloveyou.png","/static/img/hellboy.png", 
                  "/static/img/10000days.png", "/static/img/absolve.png", "/static/img/wipeout.png",
-                  "/static/img/zaba.png", "/static/img/lat.png"];
+                 "/static/img/zaba.png", "/static/img/lat.png", "/static/img/resist.png",
+                 "/static/img/cali.png","/static/img/brief.png","/static/img/crybaby.png",
+                 "/static/img/revelations.png","/static/img/atake.png"];
 var albumPicElements = document.getElementsByClassName("album-pic");
 
 
