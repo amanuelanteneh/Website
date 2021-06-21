38ddf56c6800e2bfe500018bba8ada3b41c2f1a1
amanuelanteneh <asa2rc@virginia.edu>
2021-06-20 20:40:53 -0400

    more music

--- static/js/musicScript.js
+++ static/js/musicScript.js
@@ -7,7 +7,9 @@ var albumPics = ["/static/img/the1975.png","/static/img/loveitwhenu.png","/stati
                  "/static/img/10000days.png", "/static/img/absolve.png", "/static/img/wipeout.png",
                  "/static/img/zaba.png", "/static/img/lat.png", "/static/img/resist.png",
                  "/static/img/cali.png","/static/img/brief.png","/static/img/crybaby.png",
-                 "/static/img/revelations.png","/static/img/atake.png"];
+                 "/static/img/revelations.png","/static/img/atake.png", "https://upload.wikimedia.org/wikipedia/en/2/2d/Moral_Panic_Nothing_But_Thieves.jpg",
+                "https://images-na.ssl-images-amazon.com/images/I/81Bbs8DYIHL._AC_SX522_.jpg", "https://i.scdn.co/image/ab67616d0000b27382fed5cf43d9b874184c6e00",
+                "/static/img/origin.png", "https://upload.wikimedia.org/wikipedia/en/7/7d/TheOff-Season.jpeg", "https://m.media-amazon.com/images/I/A1AHAMAExuL._SS500_.jpg"];
 var albumPicElements = document.getElementsByClassName("album-pic");
 
 
