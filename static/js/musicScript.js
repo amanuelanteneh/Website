6b984bd9b77cead6f5bd2579b15baa3af0eacf3e
amanuelanteneh <asa2rc@virginia.edu>
2021-08-11 11:32:52 -0400

    new music

--- static/js/musicScript.js
+++ static/js/musicScript.js
@@ -7,11 +7,12 @@ var albumPics = ["/static/img/the1975.png","/static/img/loveitwhenu.png","/stati
                  "/static/img/10000days.png", "/static/img/absolve.png", "/static/img/wipeout.png",
                  "/static/img/zaba.png", "/static/img/lat.png", "/static/img/resist.png",
                  "/static/img/cali.png","/static/img/brief.png","/static/img/crybaby.png",
-                 "/static/img/revelations.png","/static/img/atake.png", "https://upload.wikimedia.org/wikipedia/en/2/2d/Moral_Panic_Nothing_But_Thieves.jpg",
+                 "/static/img/revelations.png","/static/img/atake.png", "https://images.genius.com/7ccf653b4938db1f282adad9906fe0a9.1000x1000x1.jpg",
                 "https://images-na.ssl-images-amazon.com/images/I/81Bbs8DYIHL._AC_SX522_.jpg", "https://i.scdn.co/image/ab67616d0000b27382fed5cf43d9b874184c6e00",
-                "/static/img/origin.png", "https://upload.wikimedia.org/wikipedia/en/7/7d/TheOff-Season.jpeg", "https://m.media-amazon.com/images/I/A1AHAMAExuL._SS500_.jpg",
+                "/static/img/origin.png", "https://images.genius.com/439279ef55cbef2bad0530aecda4f9d8.1000x1000x1.png", "https://m.media-amazon.com/images/I/A1AHAMAExuL._SS500_.jpg",
                 "https://images.genius.com/c8a0dba90778be71f3da065544473e13.1000x1000x1.jpg","https://images.genius.com/5dedb47d3cd04901ffe7be87ecf9e10a.600x600x1.jpg",
-                "https://images.genius.com/d7d5d63b708a1b83daf2df4a1b6bdcd9.1000x1000x1.jpg"];
+                "https://images.genius.com/d7d5d63b708a1b83daf2df4a1b6bdcd9.1000x1000x1.jpg", "https://images.genius.com/de6e7ab3dc06c13eed2f853493b1406e.1000x1000x1.jpg",
+                "https://images.genius.com/5aa2f592807966cc17cd3a712d3e2784.1000x1000x1.png", "https://images.genius.com/cb21b9bd8998b4265c6cbcae597688d1.1000x1000x1.jpg"];
 var albumPicElements = document.getElementsByClassName("album-pic");
 
 
