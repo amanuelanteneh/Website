690882f8c5bfd7eaa57063fc1905ca4f6d0b3d8a
amanuelanteneh <asa2rc@virginia.edu>
2021-08-12 13:38:45 -0400

    app bubble change to hover

--- static/js/appBubbleScript.js
+++ static/js/appBubbleScript.js
@@ -1,6 +1,6 @@
 function appBubbleFunction() {
     var clickCount = 0;
-    $('#appBubble').click(function () { //fix because someone can span tf outta this and break it
+    $('#appBubble').hover(function () { //fix because someone can span tf outta this and break it
         if (clickCount % 2 == 0) {
             $('#appBubble').animate({ height: '55vh' }) //extend height on even clicks
              clickCount++; 
