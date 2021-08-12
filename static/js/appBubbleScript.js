e8f9bfd3d8cc8e72d3f3518063594ce97946ad61
amanuelanteneh <asa2rc@virginia.edu>
2021-08-12 13:41:17 -0400

    app bubble change to hover

--- static/js/appBubbleScript.js
+++ static/js/appBubbleScript.js
@@ -1,15 +1,15 @@
 function appBubbleFunction() {
     var clickCount = 0;
-    $('#appBubble').hover(function () { //fix because someone can span tf outta this and break it
-        if (clickCount % 2 == 0) {
+    $('#appBubble').mouseenter(function () { //fix because someone can span tf outta this and break it
             $('#appBubble').animate({ height: '55vh' }) //extend height on even clicks
-             clickCount++; 
-        }
-        else {
-            $('#appBubble').animate({ height: '5vh' }) //revert height to normal on odd num clicks
-            clickCount++; 
-        }
     });
+
+    $('#appBubble').mouseleave(function () { //fix because someone can span tf outta this and break it
+            $('#appBubble').animate({ height: '5vh' }) //revert height to normal on odd num clicks
+    });
+
 }
+
+
 $(document).ready(appBubbleFunction);
 
